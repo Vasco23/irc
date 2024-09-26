747f6614a3bc53e92368fb63aae371831cdfd1a7
isbraz <ismael.braz03@hotmail.com>
2024-09-26 17:32:20 +0100

    Bot kicking user

--- src/server/bot.cpp
+++ src/server/bot.cpp
@@ -1,5 +1,12 @@
 #include "../inc/Bot.hpp"
 
+void Bot::kickUser(Client &client, channel &channel, Server &server){
+    if (server.is_client_on_channel(client, channel)){
+        server.send_to_all_channel(":Bot KICK " + channel.get_name() + " " + client.get_nickname() + " :You have been kicked by the bot!", channel);
+        channel.remove_client(&client);
+    }
+}
+
 bool Bot::verifyMessage(std::string message, Client &client, channel &channel, Server &server) {
     std::ifstream file("src/server/forbiddenWords.txt");
     std::string line;
@@ -10,10 +17,11 @@ bool Bot::verifyMessage(std::string message, Client &client, channel &channel, S
     }
 
     while (std::getline(file, line)) {
-        // if (client.getBadBehavior() > 2){
-        //     // kickUser(client);
-        // }
-
+        if (client.getBadBehavior() > 2 &&  !server.is_client_op(client, channel)) {
+            std::cout << "Kicking user" << std::endl;
+            kickUser(client, channel, server);
+            return true;
+        }
         if (message.find(line) != std::string::npos) {
             client.incrementBadBehavior();
             server.send_to_all_channel(":Bot PRIVMSG " + channel.get_name() + " " + client.get_nickname() + " has said a forbidden expression! If you keep with this behavior, you will be kicked!", channel);
